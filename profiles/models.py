e9e8a2894fd89333d2e70cf75248f6a6bb433b0c
rohitgeddam <rohitgeddam2018@gmail.com>
2022-03-19 09:33:12 +0530

    add profile create middleware

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, default="")
     
+    def __str__(self):
+        return "{}".format(self.user.phone_number)
